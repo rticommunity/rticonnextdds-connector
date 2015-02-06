76159d8bc991a905c1dada7b33f5131955c0bc19
Gianpiero <gianpiero@brilliant-labs.com>
2015-02-06 13:18:46 -0800

    update license

--- examples/nodered/rti/RTIConnectorOut.js
+++ examples/nodered/rti/RTIConnectorOut.js
@@ -1,9 +1,15 @@
-/**
- * Copyright 2014 RTI
- *
- * Author: Gianpiero Napoli
- * email: gianpiero@rti.com
- **/
+/*****************************************************************************
+*    (c) 2005-2015 Copyright, Real-Time Innovations, All rights reserved.    *
+*                                                                            *
+* RTI grants Licensee a license to use, modify, compile, and create          *
+* derivative works of the Software.  Licensee has the right to distribute    *
+* object form only for use with RTI products. The Software is provided       *
+* "as is", with no warranty of any type, including any warranty for fitness  *
+* for any purpose. RTI is under no obligation to maintain or support the     *
+* Software.  RTI shall not be liable for any incidental or consequential     *
+* damages arising out of the use or inability to use the software.           *
+*                                                                            *
+******************************************************************************/
 
 module.exports = function(RED) {
     "use strict";
