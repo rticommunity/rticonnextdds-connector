f277621999597b0ebbb0f34267f5194f3accef83
Jeff Shimbo <jts@rti.com>
2015-12-15 16:30:26 -0800

    Update copyright of example files.

--- examples/nodejs/web_http/reader_web_event.js
+++ examples/nodejs/web_http/reader_web_event.js
@@ -1,15 +1,8 @@
-/*****************************************************************************
-*    (c) 2005-2015 Copyright, Real-Time Innovations, All rights reserved.    *
-*                                                                            *
-* RTI grants Licensee a license to use, modify, compile, and create          *
-* derivative works of the Software.  Licensee has the right to distribute    *
-* object form only for use with RTI products. The Software is provided       *
-* "as is", with no warranty of any type, including any warranty for fitness  *
-* for any purpose. RTI is under no obligation to maintain or support the     *
-* Software.  RTI shall not be liable for any incidental or consequential     *
-* damages arising out of the use or inability to use the software.           *
-*                                                                            *
-******************************************************************************/
+/*
+ * Copyright (c) 2005-2015 Real-Time Innovations, Inc. All rights reserved.
+ * Permission to modify and use for internal purposes granted.
+ * This software is provided "as is", without warranty, express or implied.
+ */
 
 /* DDS Connector
  */
@@ -35,7 +28,7 @@ connector.on('on_data_available', function() {
 
 var http = require('http');
 
-http.createServer(function (req, res) { 
+http.createServer(function (req, res) {
 
 	res.writeHead(200, {'Content-Type': 'text/plain'});
     for (i=1; i <= input.samples.getLength(); i++) {
@@ -44,7 +37,7 @@ http.createServer(function (req, res) {
         }
     }
     res.end();
-    
+
 }).listen(7400, '127.0.0.1');
 
 console.log('Server running at http://127.0.0.1:7400/');
