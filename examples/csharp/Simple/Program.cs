eb0c56e56b67bab1b95945b4575eacaf1527922c
Benito Palacios Sanchez <benito@rti.com>
2017-05-23 01:08:00 +0200

    [C#] Add writer example

---
+++ examples/csharp/Simple/Program.cs
@@ -0,0 +1,39 @@
+﻿// (c) Copyright, Real-Time Innovations, 2017.
+// All rights reserved.
+//
+// No duplications, whole or partial, manual or electronic, may be made
+// without express written permission.  Any such copies, or
+// revisions thereof, must display this notice unaltered.
+// This code contains trade secrets of Real-Time Innovations, Inc.
+namespace Simple
+{
+    using System;
+    using System.Threading;
+    using RTI.Connector;
+
+    class MainClass
+    {
+        public static void Main()
+        {
+            string configPath = "ShapeExample.xml";
+            string configName = "MyParticipantLibrary::Zero";
+
+            using (var connector = new Connector(configName, configPath))
+                Publish(connector);
+        }
+
+        static void Publish(Connector connector)
+        {
+            string writerName = "MyPublisher::MySquareWriter";
+            Writer writer = new Writer(connector, writerName);
+
+            Instance instance = writer.Instance;
+            for (int i = 0; i < 5; i++) {
+                Console.WriteLine("Writing sample {0}", i);
+                instance["x"] = i;
+                writer.Write();
+                Thread.Sleep(2000);
+            }
+        }
+    }
+}
---
+++ examples/csharp/Simple/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿// (c) Copyright, Real-Time Innovations, 2016.
+// All rights reserved.
+//
+// No duplications, whole or partial, manual or electronic, may be made
+// without express written permission.  Any such copies, or
+// revisions thereof, must display this notice unaltered.
+// This code contains trade secrets of Real-Time Innovations, Inc.
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RTI Connext DDS Connector")]
+[assembly: AssemblyDescription("Example usage of .NET API Connector")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Real-Time Innovations, Inc.")]
+[assembly: AssemblyProduct("RTI Connext DDS")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: CLSCompliant(true)]
+[assembly: ComVisible(true)]
