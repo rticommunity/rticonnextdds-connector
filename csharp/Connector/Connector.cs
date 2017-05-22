16718f81284078eaaf49ec204019225b062f9eab
Benito Palacios Sanchez <benito@rti.com>
2017-05-22 19:11:40 +0200

    Implement Connector wrapper in C#

---
+++ csharp/Connector/Connector.cs
@@ -0,0 +1,58 @@
+﻿// (c) Copyright, Real-Time Innovations, 2017.
+// All rights reserved.
+//
+// No duplications, whole or partial, manual or electronic, may be made
+// without express written permission.  Any such copies, or
+// revisions thereof, must display this notice unaltered.
+// This code contains trade secrets of Real-Time Innovations, Inc.
+namespace RTI.Connector
+{
+    using System;
+
+    public class Connector : IDisposable
+    {
+        readonly Interface.Connector internalConnector;
+
+        public Connector(string configName, string configFile)
+        {
+            ConfigName = configName;
+            ConfigFile = configFile;
+
+            internalConnector = new Interface.Connector(configName, configFile);
+        }
+
+        ~Connector()
+        {
+            Dispose(false);
+        }
+
+        public string ConfigName {
+            get;
+            private set;
+        }
+
+        public string ConfigFile {
+            get;
+            private set;
+        }
+
+        public bool Disposed {
+            get;
+            private set;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        void Dispose(bool freeManagedResources)
+        {
+            if (Disposed)
+                return;
+
+            Disposed = true;
+        }
+    }
+}
---
+++ csharp/Connector/Interface/Connector.cs
@@ -0,0 +1,82 @@
+﻿// (c) Copyright, Real-Time Innovations, 2017.
+// All rights reserved.
+//
+// No duplications, whole or partial, manual or electronic, may be made
+// without express written permission.  Any such copies, or
+// revisions thereof, must display this notice unaltered.
+// This code contains trade secrets of Real-Time Innovations, Inc.
+namespace RTI.Connector.Interface
+{
+    using System;
+    using System.Runtime.InteropServices;
+    using System.Security;
+
+    sealed class Connector : IDisposable
+    {
+        readonly ConnectorPtr handle;
+
+        public Connector(string configName, string configFile)
+        {
+            handle = new ConnectorPtr(configName, configFile);
+        }
+
+        ~Connector()
+        {
+            Dispose(false);
+        }
+
+        public bool Disposed {
+            get;
+            private set;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        void Dispose(bool freeManagedResources)
+        {
+            if (Disposed)
+                return;
+
+            Disposed = true;
+            if (freeManagedResources && !handle.IsInvalid)
+                handle.Dispose();
+        }
+
+        sealed class ConnectorPtr : SafeHandle
+        {
+            public ConnectorPtr(string configName, string configFile)
+                : base(IntPtr.Zero, true)
+            {
+                handle = SafeNativeMethods.RTIDDSConnector_new(
+                    configName,
+                    configFile,
+                    IntPtr.Zero);
+            }
+
+            public override bool IsInvalid => handle == IntPtr.Zero;
+
+            protected override bool ReleaseHandle()
+            {
+                SafeNativeMethods.RTIDDSConnector_delete(handle);
+                return true;
+            }
+        }
+
+        [SuppressUnmanagedCodeSecurity]
+        static class SafeNativeMethods
+        {
+            [DllImport("librti_dds_connector")]
+            public static extern IntPtr RTIDDSConnector_new(
+                string configName,
+                string configFile,
+                IntPtr config);
+
+            [DllImport("librti_dds_connector")]
+            public static extern void RTIDDSConnector_delete(IntPtr handle);
+        }
+    }
+}
