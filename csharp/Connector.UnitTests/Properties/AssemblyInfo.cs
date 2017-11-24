ce2b96987fb754199c966ec58e73761a232c6b94
Benito Palacios Sanchez <benito@rti.com>
2017-11-24 13:13:33 +0100

    [C#] Add solution for older VS

---
+++ csharp/Connector.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿// (c) Copyright, Real-Time Innovations, 2017.
+// All rights reserved.
+//
+// No duplications, whole or partial, manual or electronic, may be made
+// without express written permission.  Any such copies, or
+// revisions thereof, must display this notice unaltered.
+// This code contains trade secrets of Real-Time Innovations, Inc.
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RTI Connext DDS Connector UnitTests")]
+[assembly: AssemblyDescription(".NET UnitTests for API for RTI Connector")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Real-Time Innovations, Inc.")]
+[assembly: AssemblyProduct("RTI Connext DDS Connector")]
+[assembly: AssemblyCopyright("(c) Copyright, Real-Time Innovations, 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.0")]
+[assembly: CLSCompliant(true)]
+[assembly: ComVisible(true)]
